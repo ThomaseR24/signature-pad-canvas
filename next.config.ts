ee956e3815224e9e2152e05e83e4ae08dd930377
Thomas er24 <rehmer@e-recht24.de>
2025-01-20 18:49:06 +0100

    remove cache

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // Cache deaktivieren
+  staticPageGenerationTimeout: 0,
+  generateEtags: false
 };
 
 export default nextConfig;
